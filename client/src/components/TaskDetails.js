b4acff60d7b3398e94d838d8f1667e3a451db471
The-EleetCoder <jaijain1803@gmail.com>
2024-07-03 18:03:09 +0530

    made TaskDetails

--- client/src/components/TaskDetails.js
+++ client/src/components/TaskDetails.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const TaskDetails = () => {
-  return (
-    <div>TaskDetails</div>
-  )
-}
+  const [task, setTask] = useState({});
+  const { id } = useParams();
+  console.log(process.env.REACT_APP_BASE_URL);
 
-export default TaskDetails
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_BASE_URL + `/api/tasks/${id}`)
+      .then((response) => setTask(response.data))
+      .catch((error) =>
+        console.error("There was an error fetching the task!", error)
+      );
+  }, [id]);
+
+  return (
+    <div className="container">
+      <h1 className="my-4">Task Details</h1>
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+      <p>
+        <strong>Due Date:</strong> {task.dueDate}
+      </p>
+    </div>
+  );
+};
+
+export default TaskDetails;
